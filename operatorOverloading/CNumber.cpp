c2ec0444117a23211d718050be45858dc61d71d5
hardwak <ymhere2@gmail.com>
2023-11-02 21:14:48 +0100

    + operator fixed

--- operatorOverloading/CNumber.cpp
+++ operatorOverloading/CNumber.cpp
@@ -4,10 +4,10 @@
 #include <iostream>
 #include "CNumber.h"
 
-void oo_test(){
+void oo_test() {
     CNumber c_num_0, c_num_1;
-    c_num_0 = 21474;
-    c_num_1 = 2147483647;
+    c_num_0 = 10;
+    c_num_1 = 990;
 
     std::cout << c_num_0.sToStr() << " " << c_num_1.sToStr() << std::endl;
 
@@ -18,13 +18,13 @@ void oo_test(){
     std::cout << c_num_0 << " " << c_num_1 << std::endl;
 }
 
-CNumber& CNumber::operator=(const int iValue) {
+CNumber &CNumber::operator=(const int iValue) {
     if (tab)
         delete[]tab;
 
     std::string number = std::to_string(iValue);
 
-    tab = new int [number.size()];
+    tab = new int[number.size()];
     //why static cast
     size = static_cast<int>(number.size());
 
@@ -36,11 +36,11 @@ CNumber& CNumber::operator=(const int iValue) {
 
 }
 
-CNumber& CNumber::operator=(const CNumber &pcOther) {
+CNumber &CNumber::operator=(const CNumber &pcOther) {
 //    tab = pcOther.tab;
 //    size = pcOther.size;
 
-    if (&pcOther == this){
+    if (&pcOther == this) {
         return *this;
     }
 
@@ -55,26 +55,35 @@ CNumber& CNumber::operator=(const CNumber &pcOther) {
         tab[i] = pcOther.tab[i];
     }
 
-       return *this;
+    return *this;
 }
 
 CNumber CNumber::operator+(CNumber &other) {
+    CNumber *bigger = this;
+    CNumber *smaller = &other;
+    if (this->size < other.size) {
+        bigger = &other;
+        smaller = this;
+    }
+
     CNumber newNumber;
     delete[] newNumber.tab;
-    newNumber.tab = new int [std::max(this->size, other.size)];
-    newNumber.size = std::max(this->size, other.size);
-    
+    newNumber.tab = new int[bigger->size];
+    newNumber.size = bigger->size;
+
     int singleNum;
     int overTen = 0;
-    for (int i = 0; i < std::min(this->size, other.size); ++i) {
-        singleNum = this->tab[this->size - 1 - i] + other.tab[other.size - 1 - i] + overTen;
+    for (int i = 0; i < bigger->size; ++i) {
+        singleNum = bigger->tab[bigger->size - 1 - i] + overTen;
+        if (smaller->size - 1 - i >= 0)
+            singleNum += smaller->tab[smaller->size - 1 - i];
 
-        newNumber.tab[size - 1 - i] = singleNum % 10;
+        newNumber.tab[newNumber.size - 1 - i] = singleNum % 10;
 
         overTen = singleNum >= 10 ? 1 : 0;
     }
 
-    if (this->size == other.size && overTen == 1){
+    if (overTen == 1) {
         int *newTab = new int[size + 1];
         newTab[0] = 1;
         for (int i = 1; i < size + 1; ++i) {
@@ -86,38 +95,21 @@ CNumber CNumber::operator+(CNumber &other) {
         newNumber.size++;
     }
 
-    if (this->size != other.size){
-        CNumber* bigger;
-        CNumber* smaller;
-        if (this->size > other.size){
-            bigger = this;
-            smaller = &other;
-        } else{
-            bigger = &other;
-            smaller = this;
-        }
-
-        int posOfDiff = bigger->size - smaller->size - 1;
-        for (int i = 0; i <= posOfDiff; ++i) {
-            newNumber.tab[posOfDiff - i] = bigger->tab[posOfDiff - i] + overTen;
-            overTen = 0;
-        }
-    }
 
     return newNumber;
 }
 
-std::ostream& operator<<(std::ostream& os, const CNumber& obj){
+std::ostream &operator<<(std::ostream &os, const CNumber &obj) {
     std::string stringToReturn;
     for (int i = 0; i < obj.size; ++i) {
-        stringToReturn += std::to_string(obj.tab[i]) + " ";
+        stringToReturn += std::to_string(obj.tab[i]);
 
     }
     os << stringToReturn;
     return os;
 }
 
-std::string CNumber::sToStr(){
+std::string CNumber::sToStr() {
     std::string stringToReturn;
     for (int i = 0; i < size; ++i) {
         stringToReturn += std::to_string(tab[i]);
